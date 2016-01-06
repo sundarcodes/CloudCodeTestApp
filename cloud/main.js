ab10c38a392091d3f83224495857e9e7f1396cd0
sundarcodes <sundar82@gmail.com>
2016-01-06 18:17:02 +0530

    added Email sending function in cloude code

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,23 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!..its superb");
 });
+
+Parse.Cloud.define("sendTestMail", function() {
+    var Mailgun = require('mailgun');
+	Mailgun.initialize('https://api.mailgun.net/v3/sandboxdc19f76f0daa463c9a2ff5eb0a5f0975.mailgun.org/messages', 'key-5d9487f0c64aad70b2155562eb91b613');
+	Mailgun.sendEmail({
+	  to: "sundar82@gmail.com",
+	  from: "sam@testMail.com",
+	  subject: "Hello from Mail Gun!",
+	  text: "Using Parse and Mailgun is super fun!"
+	}, {
+	  success: function(httpResponse) {
+	    console.log(httpResponse);
+	    response.success("Email sent!");
+	  },
+	  error: function(httpResponse) {
+	    console.error(httpResponse);
+	    response.error("Uh oh, something went wrong");
+	  }
+	});
+});
