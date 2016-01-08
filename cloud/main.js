cc61353523804e9c930699e5c395b686921f7746
sundarcodes <sundar82@gmail.com>
2016-01-08 15:23:01 +0530

    Changed Default App to Skool and added afterSave trigger for Student to create parents as users and send mail to them

--- cloud/main.js
+++ cloud/main.js
@@ -6,7 +6,7 @@ Parse.Cloud.define("hello", function(request, response) {
 });
 
 Parse.Cloud.define("sendTestMail", function(request,response) {
-    var Mailgun = require('mailgun');
+  var Mailgun = require('mailgun');
 	Mailgun.initialize('sandboxdc19f76f0daa463c9a2ff5eb0a5f0975.mailgun.org', 'key-5d9487f0c64aad70b2155562eb91b613');
 	Mailgun.sendEmail({
 	  to: "sundar82@gmail.com",
@@ -24,3 +24,51 @@ Parse.Cloud.define("sendTestMail", function(request,response) {
 	  }
 	});
 });
+
+Parse.Cloud.afterSave("Student", function(request) {
+  var user = new Parse.User();
+  var parentEmail = request.object.get("Student").parentEmail;
+  console.log(parentEmail);
+  user.set("username", parentEmail );
+  var randomPassword = generateRandomPassword();
+  user.set("password", randomPassword);
+  // user.set("email", "email@example.com");
+
+  user.signUp(null, {
+    success: function(user) {
+      // Hooray! Let them use the app now.
+      // Send them a email
+      var emailBody = "Dear Parent,\n Congratulations. You have successfully registered for the skoolApp. Your username is "+parentEmail+" and password is "+randomPassword;
+      sendMail(parentEmail,"yourFriend@skoopApp.com","Successful Registration",emailBody);
+    },
+    error: function(user, error) {
+      // Show the error message somewhere and let the user try again.
+      console.log("Error: " + error.code + " " + error.message);
+    }
+  });
+
+});
+
+generateRandomPassword = function(){
+  return "password";
+}
+
+sendMail = function(toUser,fromUser,emailSubject,emailBody) {
+  var Mailgun = require('mailgun');
+	Mailgun.initialize('sandboxdc19f76f0daa463c9a2ff5eb0a5f0975.mailgun.org', 'key-5d9487f0c64aad70b2155562eb91b613');
+	Mailgun.sendEmail({
+	  to: toUser,
+	  from: fromUser,
+	  subject: emailSubject,
+	  text: emailBody
+	}, {
+	  success: function(httpResponse) {
+	    console.log(httpResponse);
+	    console.log("Email sent! to :"+ toUser);
+	  },
+	  error: function(httpResponse) {
+	    console.error(httpResponse);
+	    console.log("Uh oh, something went wrong");
+	  }
+	});
+}
